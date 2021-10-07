9c93ef56c2c5bb99dd476af26fb27d9eeff22604
Thomas <thomas1.reicher@gmail.com>
2021-10-07 09:55:07 +0200

    commit

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -38,7 +38,7 @@ window.addEventListener('load', function(e) {
 
 	
 	setTimeout(function(){
-		burger.dispatchEvent(clickEvent);
+		
 		
 		setTimeout(function(){
 			slowmo.dispatchEvent(clickEvent);
